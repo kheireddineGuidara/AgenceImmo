9493721b748c2e52de9239d973dbc234d595f507
kheireddineGuidara <kheireddineguidara@gmail.com>
2024-06-03 11:25:51 +0100

    first commit

---
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\PropertyFormRequest;
+use App\Models\Property;
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+
+    public function index()
+    {
+        return view('admin.properties.index', [
+            'properties' => Property::orderBy('created_at', 'desc')->paginate(25),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.properties.form', [
+            'property' => new Property()
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PropertyFormRequest $request)
+    {
+        $property = Property::create($request->validated());
+        return to_route('admin.property.index')->with('success','Le bien a bien été créer');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
