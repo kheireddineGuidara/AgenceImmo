443354a5f1423716177d3a819218634035d86d0d
kheireddineGuidara <kheireddineguidara@gmail.com>
2024-06-05 12:38:39 +0100

    Accesseurs et Mutaateurs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Property;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +11,16 @@ class HomeController extends Controller
 
     public function index()
     {
-        $properties = Property::orderBy('created_at','desc')->limit(4)->get();
+        $properties = Property::available()->orderBy('created_at','desc')->limit(4)->get();
+        /** @var User $user */
+        $user = User::first();
+        $user->password = '123456';
+        $user->syncChanges();
+        dd($user->password, $user);
         return view('home', ['properties' => $properties]);
+    }
+
+    private function available()
+    {
     }
 }
--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
-use Dotenv\Util\Str;
+
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Property extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title',
@@ -32,6 +36,11 @@ class Property extends Model
 
     public function getSlug(): string
     {
-        return \Illuminate\Support\Str::slug($this->title);
+        return Str::slug($this->title);
+    }
+
+    public function scopeAvailable(Builder $builder): Builder
+    {
+        return $builder->where('sold',false);
     }
 }
