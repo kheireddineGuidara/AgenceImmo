e88e65609bed27bf25faedf7e6e8239a66a0607b
kheireddineGuidara <kheireddineguidara@gmail.com>
2024-07-03 12:50:00 +0100

    introducing filament

--- app/Filament/Resources/PropertyResource.php
+++ app/Filament/Resources/PropertyResource.php
@@ -114,6 +114,7 @@ class PropertyResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
                 Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
